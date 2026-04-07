4183e71214d0e4c32cb9ab557d1b115e349593ee
Donovan <donovan@example.com>
2026-04-07 00:31:04 -0600

    Reiniciando migraciones para PostgreSQL en Render

--- backend/prisma/migrations/20251127001948_add_payment_method/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Payment` ADD COLUMN `paymentMethod` ENUM('EFECTIVO', 'TARJETA') NOT NULL DEFAULT 'EFECTIVO';
--- backend/prisma/migrations/20251209215133_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Payment` ALTER COLUMN `updatedAt` DROP DEFAULT;
